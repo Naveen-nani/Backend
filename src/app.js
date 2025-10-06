7c6ad85c1b5dd18ca7a763b7b30d64dbb51a3bbd
naveen <naveennanigande422@gmail.com>
2025-10-06 06:59:14 +0530

    try all http calls thru postman

--- src/app.js
+++ src/app.js
@@ -3,13 +3,27 @@
 const express = require('express');
 const app = express();
 
-app.use('/getData',(req,res) => {
-    res.send('Hello world');
+app.get('/getData',(req,res) => {
+    res.send({name:'naveen', age:24, city:'hyd'})
+  })
+
+app.post('/getData',(req,res) => {
+  res.send({name:'naveen', age:24})
 })
 
-app.use('/updateuser',(req,res) => {
-    res.send('naveen gande');
+app.delete('/getData', (req, res) => {
+    res.send({ message: 'data deleted successfully' })
 })
+
+
+
+
+
+
+// work like default route.
+app.use('/',(req,res) => {
+    res.send('hello from naveen Gande ......!')
+})  
 
 app.listen(7777, () => {
     console.log('Server is running on http://localhost:7777');
--- src/notes.md
+++ src/notes.md
@@ -8,3 +8,10 @@
 - `app.listen(port, callback)` starts the server on the specified port
 - Test the server by visiting `http://localhost:<port>` in the browser or using tools like Postman
 - Use `console.log()` to debug and log server activity.
+
+- routing play with routes and extentions.
+- Install postman and use it. with all methods.
+- workspace/collection - test api calls. 
+- try examples with get , POST , put and delete calls
+- try in postman.
+- 
